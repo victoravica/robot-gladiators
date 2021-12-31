67dca9f080c66840cd644bf14a75eba89a902eb3
Victor Alvarado <victoralvaradolopez@gmail.com>
2021-12-30 16:57:40 -0800

    added prompt to enter robot name and stored the variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+
+//This will do math and log 20
+console.log(10 + 10);
+
+//What is this?
+
+console.log("The best robot ever is " + playerName);
+
+console.log("The number on robot is " + playerName)
+
+// just a function example  window.alert("This is an alert! Javascript is running!");
+
+//to declare a function
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//to call a function
+
+//fight();
